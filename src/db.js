06f3ce9c1e2c8531ad4d672f2b7598efce79dd58
MrMiniblack <119525269+MrMiniblack@users.noreply.github.com>
2025-02-27 00:43:36 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,8 @@
-
 const fs = require('fs');
 const path = require('path');
 
 const dbFilePath = path.join(__dirname, 'db.json');
 
-// Function to read data from db.json
 function readData() {
   if (!fs.existsSync(dbFilePath)) {
     return { bots: [], reactions: {} };
@@ -13,12 +11,10 @@ function readData() {
   return JSON.parse(rawData);
 }
 
-// Function to write data to db.json
 function writeData(data) {
   fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2));
 }
 
-// Function to add a bot
 function addBot(botToken) {
   let db = readData();
   if (!db.bots.includes(botToken)) {
@@ -27,27 +23,23 @@ function addBot(botToken) {
   }
 }
 
-// Function to remove a bot
 function removeBot(botToken) {
   let db = readData();
   db.bots = db.bots.filter(token => token !== botToken);
-  delete db.reactions[botToken]; // Also remove reactions
+  delete db.reactions[botToken];
   writeData(db);
 }
 
-// Function to get all bots
 function getBots() {
   return readData().bots;
 }
 
-// Function to set reactions
 function setReactions(botToken, reactions) {
   let db = readData();
   db.reactions[botToken] = reactions;
   writeData(db);
 }
 
-// Function to get reactions
 function getReactions(botToken) {
   return readData().reactions[botToken] || [];
 }
