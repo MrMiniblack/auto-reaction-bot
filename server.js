9f5eea158226f4085a86dcfa198e5c4649aa263b
MrMiniblack <119525269+MrMiniblack@users.noreply.github.com>
2025-02-27 00:42:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const { loadBots } = require('./bot');
-const routes = require('./routes');
+const { loadBots } = require('./src/bot');
+const routes = require('./src/routes');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+
 app.use(express.json());
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  await loadBots();
+  loadBots();
 });
